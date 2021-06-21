0e6eed59bbc59fc42d1b048997affa5a865a5f8a
Mistra <sperectrum@gmail.com>
2021-06-22 00:14:44 +0200

    modified config files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,3 @@
-CREATE TABLE IF NOT EXISTS rating (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    movie_id INT NOT NULL,
-    score INT NOT NULL
-);
-
 INSERT INTO rating (id, user_id, movie_id, score) VALUES
   (1, 1, 1, 5),
   (2, 1, 2, 3),
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS rating (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    score INT NOT NULL
+);
